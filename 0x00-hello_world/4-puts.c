b402d4c391c5e3ad5bc6b9cfa5e997ef016b6075
kodacom <abiloyey@gmail.com>
2023-06-15 17:17:04 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main - printing the puts function
+ * main - printing puts function on a new line
  * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return (0`);
 }
