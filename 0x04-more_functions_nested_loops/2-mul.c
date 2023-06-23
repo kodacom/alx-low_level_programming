2e99f7ec830dcb269d2872a347c879b548260081
kodacom <abiloyey@gmail.com>
2023-06-22 18:59:06 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multyiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
